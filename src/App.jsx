77d3e6ccb8e250aa89272e94913d0e2c7b7a42b4
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2019-01-18 23:41:49 +0530

    Added events and resources fetching

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import OpenProjects from './pages/OpenProjects';
 import Showcase from './pages/Showcase';
 import firestore from './helpers/firebase';
 import Admin from './pages/Admin';
+import Events from './pages/Events';
 
 class App extends React.Component {
   constructor(props) {
@@ -17,6 +18,8 @@ class App extends React.Component {
       showcase: [],
       openProjects: [],
       about: [],
+      resources: {},
+      events: [],
 
       errorShowcase: false,
       errorTeamMembers: false,
@@ -27,6 +30,7 @@ class App extends React.Component {
       isLoadingTeamMembers: true,
       isLoadingOpenProjects: true,
       isLoadingAbout: true,
+      isLoadingEvents: true,
     };
   }
 
@@ -79,8 +83,49 @@ class App extends React.Component {
         console.log('Error getting documents: ', error);
       });
 
-    // ref = firestore.collection('open-projects');
-    // projects.forEach((mem) => { ref.add(mem); });
+    ref = firestore.collection('events');
+    ref.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.setState(prevState => ({ events: [...prevState.events, doc.data()] }));
+      });
+      this.setState({ isLoadingEvents: false });
+    })
+      .catch((error) => {
+        console.log('Error getting documents: ', error);
+      });
+
+    // Need to do this due to some error in firestore not allowing to call getCollections on a doc.
+    const subCollections = ['assignments', 'lectures', 'orientation', 'recruitment'];
+    ref = firestore.collection('resources');
+    ref.get().then((collectionSnap) => {
+      collectionSnap.forEach((directory) => {
+        const data = directory.data();
+        subCollections.forEach((subCollection) => {
+          const subRef = firestore.collection(`${data.path}/${data.doc_name}/${subCollection}`);
+          subRef.get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              const documentData = doc.data();
+
+              this.setState((prevState) => {
+                const dataRes = JSON.parse(JSON.stringify(prevState.resources));
+                if (dataRes[data.doc_name] === undefined) {
+                  dataRes[data.doc_name] = {};
+                }
+                if (dataRes[data.doc_name][subCollection] === undefined) {
+                  dataRes[data.doc_name][subCollection] = {};
+                }
+                dataRes[data.doc_name][subCollection][documentData.doc_name] = documentData;
+                console.log(dataRes);
+                return { resources: dataRes };
+              });
+            });
+          });
+        });
+      });
+    })
+      .catch((error) => {
+        console.log('Error getting documents: ', error);
+      });
   }
 
   render() {
@@ -88,6 +133,7 @@ class App extends React.Component {
     const { teamMembers, isLoadingTeamMembers, errorTeamMembers } = this.state;
     const { openProjects, isLoadingOpenProjects, errorOpenProjects } = this.state;
     const { about, isLoadingAbout, errorAbout } = this.state;
+    const { events, isLoadingEvents } = this.state;
 
     return (
       <Router>
@@ -95,6 +141,7 @@ class App extends React.Component {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/events">Events</Link></li>
             <li>
               Projects
               <ul>
@@ -154,6 +201,17 @@ class App extends React.Component {
                   data={teamMembers}
                   isLoading={isLoadingTeamMembers}
                   error={errorTeamMembers}
+                />
+              )}
+          />
+          <Route
+            path="/events"
+            render={
+              props => (
+                <Events
+                  {...props}
+                  data={events}
+                  isLoading={isLoadingEvents}
                 />
               )}
           />
