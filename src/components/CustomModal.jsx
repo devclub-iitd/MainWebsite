e2140750ea93df7740453ea8a813249ab7f9d6ae
DivyanshuSaxena <dssaxena2011@gmail.com>
2019-04-12 03:37:59 +0530

    Minor changes in disqus button styling

--- src/components/CustomModal.jsx
+++ src/components/CustomModal.jsx
@@ -12,11 +12,7 @@ const styles = theme => ({
     width: theme.spacing.unit * 50,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
-    padding: theme.spacing.unit * 4,
     outline: 'none',
-  },
-  button: {
-    margin: theme.spacing.unit,
   },
 });
 
