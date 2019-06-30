69dd82831409aff16cfc5541bae306b76d81bb4b
Divyanshu Saxena <divyanshu.saxena@cohesity.com>
2019-07-01 02:16:41 +0530

    SVG Animation on landing Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core';
-import { useSpring, animated } from 'react-spring';
+import { withStyles, Grid } from '@material-ui/core';
+import styled, { css, keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 import Typing from 'react-typing-animation';
 
 import '../index.css';
+import css3 from '../images/css-3-pp.svg';
+import html5 from '../images/html-5-freepik.svg';
+import js from '../images/java-script-pp.svg';
+import python from '../images/python-freepik.svg';
+import bash from '../images/gnu-bash-freepik.svg';
+import ts from '../images/typescript-freepik.svg';
+import docker from '../images/docker-freepik.svg';
+import react from '../images/react-freepik.svg';
 
 const styles = () => ({
   root: {
@@ -13,6 +21,13 @@ const styles = () => ({
     marginTop: '10vh',
     willChange: 'width, height, left, top',
     marginLeft: '5vw',
+  },
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   scriptBox: {
     position: 'relative',
@@ -27,61 +42,79 @@ const styles = () => ({
   },
 });
 
+const items = [css3, html5, js, python, bash, ts, docker, react];
+const distanceX = [120, 120, 120, 120, -120, -120, -120, -120];
+const distanceY = [40, -40, 80, -80, 40, -40, 80, -80];
+
+const AnimatedSvg = styled.div`
+  width: 3.5vw;
+  height: 3.5vw;
+  min-width: 5ch;
+  min-height: 5ch;
+  position: absolute;
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  will-change: transform;
+  background-image: url(${props => props.theme['background-image']});
+  animation: ${props => props.theme.animation};
+`;
+
 function responsiveDistance(distance) {
   let ratio = window.innerWidth / 1920;
   ratio = ratio < 0.6 ? 0.6 : ratio;
   return ratio * distance;
 }
 
-const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
-const trans1 = (x, y) => `translate3d(${x / 40}px,${y / 40}px,0)`; // Desktop
-const trans2 = (x, y) => `translate3d(${x / 18 + responsiveDistance(25)}px,${y / 18 - responsiveDistance(180)}px,0)`; // CSS3
-const trans3 = (x, y) => `translate3d(${x / 18 - responsiveDistance(160)}px,${y / 18 - responsiveDistance(150)}px,0)`; // HTML5
-const trans4 = (x, y) => `translate3d(${x / 8 + responsiveDistance(80)}px,${y / 8 + responsiveDistance(10)}px,0)`; // JS
-const trans5 = (x, y) => `translate3d(${x / 18 + responsiveDistance(190)}px,${y / 18 - responsiveDistance(100)}px,0)`; // Python
-const trans6 = (x, y) => `translate3d(${x / 8 - responsiveDistance(80)}px,${y / 8 - responsiveDistance(50)}px,0)`; // Bash
-const trans7 = (x, y) => `translate3d(${x / 18 + responsiveDistance(170)}px,${y / 18 + responsiveDistance(140)}px,0)`; // TS
-const trans8 = (x, y) => `translate3d(${x / 18 - responsiveDistance(25)}px,${y / 18 + responsiveDistance(170)}px,0)`; // Docker
-const trans9 = (x, y) => `translate3d(${x / 18 - responsiveDistance(170)}px,${y / 18 + responsiveDistance(100)}px,0)`; // React
-
-function Card() {
-  const [props, set] = useSpring(() => ({
-    xy: [0, 0],
-    config: { mass: 10, tension: 550, friction: 140 },
-  }));
-  const { xy } = props;
-  return (
-    <div className="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
-      <animated.div className="card1" style={{ transform: xy.interpolate(trans1) }} />
-      <animated.div className="card2" style={{ transform: xy.interpolate(trans2) }} />
-      <animated.div className="card5" style={{ transform: xy.interpolate(trans5) }} />
-      <animated.div className="card8" style={{ transform: xy.interpolate(trans8) }} />
-      <animated.div className="card3" style={{ transform: xy.interpolate(trans3) }} />
-      <animated.div className="card4" style={{ transform: xy.interpolate(trans4) }} />
-      <animated.div className="card6" style={{ transform: xy.interpolate(trans6) }} />
-      <animated.div className="card7" style={{ transform: xy.interpolate(trans7) }} />
-      <animated.div className="card9" style={{ transform: xy.interpolate(trans9) }} />
-    </div>
-  );
+function svgTheme(index) {
+  const svgStyle = {};
+  svgStyle['background-image'] = items[index];
+  const keyFrame = keyframes`
+    0% {
+      transform: translate3d(0,0,0);
+    }
+    100% {
+      transform: translate3d(${responsiveDistance(distanceX[index])}px,${responsiveDistance(distanceY[index])}px,0);
+    }
+  `;
+  svgStyle.animation = css`
+      ${keyFrame} 2s infinite
+    `;
+  return svgStyle;
 }
 
 const Home = (props) => {
   const { classes } = props;
+  // Generate animated SVG components using items array
+  const svgArray = [];
+  for (let index = 0; index < items.length; index += 1) {
+    const svgItem = (
+      <AnimatedSvg theme={svgTheme(index)} />
+    );
+    svgArray.push(svgItem);
+  }
+
   return (
     <React.Fragment>
       <div className={classes.root}>
-        <Card />
-        <Typing className={classes.typing}>
-          <Typing.Speed ms={60} />
-          <span className={classes.line1}>This is</span>
-          <Typing.Delay ms={200} />
-          <br />
-          <Typing.Speed ms={5} />
-          <span>The Software Development Club, IIT Delhi</span>
-          <Typing.Reset count={1} delay={800} />
-          <Typing.Speed ms={20} />
-          DevClub
-        </Typing>
+        <Grid container>
+          <Grid container item md={7} className={classes.container}>
+            {svgArray}
+          </Grid>
+          <Grid container item md={5} className={classes.typingContainer}>
+            <Typing className={classes.typing}>
+              <Typing.Speed ms={60} />
+              <span className={classes.line1}>This is</span>
+              <Typing.Delay ms={200} />
+              <br />
+              <Typing.Speed ms={5} />
+              <span>The Software Development Club, IIT Delhi</span>
+              <Typing.Reset count={1} delay={800} />
+              <Typing.Speed ms={20} />
+              DevClub
+            </Typing>
+          </Grid>
+        </Grid>
       </div>
       {/* <a href="http://bit.ly/2CGChyY"> Report bugs/Suggestions.</a> */}
     </React.Fragment>
@@ -90,10 +123,6 @@ const Home = (props) => {
 
 Home.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
-};
-
-Card.propTypes = {
-  xy: PropTypes.objectOf(PropTypes.array).isRequired,
 };
 
 export default withStyles(styles)(Home);
