8fbf40d692b61cb6964fe4e973ae3ee5ecbfa062
Divyanshu Saxena <divyanshu.saxena@cohesity.com>
2019-07-03 00:20:24 +0530

    Landing Page SVG animations complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import styled, { css, keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 import Typing from 'react-typing-animation';
 
-import '../index.css';
 import css3 from '../images/css-3-pp.svg';
 import html5 from '../images/html-5-freepik.svg';
 import js from '../images/java-script-pp.svg';
@@ -14,6 +13,7 @@ import bash from '../images/gnu-bash-freepik.svg';
 import ts from '../images/typescript-freepik.svg';
 import docker from '../images/docker-freepik.svg';
 import react from '../images/react-freepik.svg';
+import devices from '../images/devices-srip.svg';
 
 const styles = () => ({
   root: {
@@ -23,22 +23,32 @@ const styles = () => ({
     willChange: 'width, height, left, top',
     marginLeft: '5vw',
   },
+  landingContainer: {
+    position: 'relative',
+    height: '80vh',
+  },
   container: {
-    width: '100%',
-    height: '100%',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  devices: {
+    position: 'absolute',
+    width: '20vw',
+    height: '20vw',
+    minWidth: '30ch',
+    minHeight: '30ch',
+    maxWidth: '80ch',
+    maxHeight: '80ch',
+    backgroundImage: `url(${devices})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+  },
   scriptBox: {
     position: 'relative',
   },
-  typingContainer: {
-    position: 'relative',
-    height: '90vh',
-  },
   typing: {
-    marginTop: '25vh',
     fontSize: '36px',
     textAlign: 'center',
     margin: 'auto',
@@ -49,15 +59,17 @@ const styles = () => ({
 });
 
 const items = [css3, html5, js, python, bash, ts, docker, react];
-const distanceX = [120, 120, 120, 120, -120, -120, -120, -120];
-const distanceY = [40, -40, 80, -80, 40, -40, 80, -80];
+const distanceX = [240, 240, 120, 120, -120, -120, -240, -240];
+const distanceY = [120, -150, 160, -100, 100, -120, 140, -80];
 
 const AnimatedSvg = styled.div`
-  width: 3.5vw;
-  height: 3.5vw;
+  width: 2.5vw;
+  height: 2.5vw;
   min-width: 5ch;
   min-height: 5ch;
-  position: absolute;
+  position: relative;
+  right: ${props => props.theme.right}px;
+  bottom: ${props => props.theme.bottom}px;
   background-size: cover;
   background-position: center center;
   background-repeat: no-repeat;
@@ -73,19 +85,27 @@ function responsiveDistance(distance) {
 }
 
 function svgTheme(index) {
-  const svgStyle = {};
-  svgStyle['background-image'] = items[index];
+  const radius = 20 + Math.random() * 10;
+  const direction = Math.random() > 0.5 ? -1 : 1;
+  const startAngle = Math.random() * 360;
+  const duration = 2 + Math.random() * 3;
   const keyFrame = keyframes`
     0% {
-      transform: translate3d(0,0,0);
+      transform:  rotate(${startAngle}deg)
+                  translate(${radius}px)
+                  rotate(${-startAngle}deg);
     }
     100% {
-      transform: translate3d(${responsiveDistance(distanceX[index])}px,${responsiveDistance(distanceY[index])}px,0);
+      transform:  rotate(${startAngle - direction * 360}deg)
+                  translateX(${radius}px)
+                  rotate(${-startAngle + direction * 360}deg);
     }
   `;
-  svgStyle.animation = css`
-      ${keyFrame} 2s infinite
-    `;
+  const svgStyle = {};
+  svgStyle['background-image'] = items[index];
+  svgStyle.animation = css`${keyFrame} ${duration}s linear infinite`;
+  svgStyle.right = responsiveDistance(distanceX[index]);
+  svgStyle.bottom = responsiveDistance(distanceY[index]);
   return svgStyle;
 }
 
@@ -103,11 +123,12 @@ const Home = (props) => {
   return (
     <React.Fragment>
       <div className={classes.root}>
-        <Grid container>
+        <Grid container className={classes.landingContainer}>
           <Grid container item md={7} className={classes.container}>
+            <div className={classes.devices} />
             {svgArray}
           </Grid>
-          <Grid container item md={5} className={classes.typingContainer}>
+          <Grid container item md={5}>
             <Typing className={classes.typing}>
               <Typing.Speed ms={60} />
               <span className={classes.line1}>This is</span>
