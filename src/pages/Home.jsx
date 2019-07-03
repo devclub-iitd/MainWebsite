75d830068c789d146fcd2d26bb593f952533399c
Divyanshu Saxena <divyanshu.saxena@cohesity.com>
2019-07-04 01:42:06 +0530

    Landing page text improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-await-in-loop */
-import React from 'react';
+import React, {
+  useRef, useState, useEffect, useCallback,
+} from 'react';
 import { withStyles, Grid } from '@material-ui/core';
 import styled, { css, keyframes } from 'styled-components';
+import { useTransition, animated } from 'react-spring';
 import PropTypes from 'prop-types';
-import Typing from 'react-typing-animation';
 
 import css3 from '../images/css-3-pp.svg';
 import html5 from '../images/html-5-freepik.svg';
@@ -27,17 +29,23 @@ const styles = () => ({
     position: 'relative',
     height: '80vh',
   },
-  container: {
+  svgContainer: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    height: '30vh',
+    margin: 'auto',
+  },
+  introContainer: {
+    height: '60vh',
+    margin: 'auto',
   },
   devices: {
     position: 'absolute',
     width: '20vw',
     height: '20vw',
-    minWidth: '30ch',
-    minHeight: '30ch',
+    minWidth: '25ch',
+    minHeight: '25ch',
     maxWidth: '80ch',
     maxHeight: '80ch',
     backgroundImage: `url(${devices})`,
@@ -48,9 +56,20 @@ const styles = () => ({
   scriptBox: {
     position: 'relative',
   },
-  typing: {
-    fontSize: '36px',
-    textAlign: 'center',
+  intro: {
+    margin: 'auto',
+  },
+  transitionsItem: {
+    overflow: 'hidden',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    fontSize: '3em',
+    fontWeight: '800',
+    willChange: 'opacity, height',
+  },
+  transitionsText: {
     margin: 'auto',
   },
   line1: {
@@ -65,8 +84,8 @@ const distanceY = [120, -150, 160, -100, 100, -120, 140, -80];
 const AnimatedSvg = styled.div`
   width: 2.5vw;
   height: 2.5vw;
-  min-width: 5ch;
-  min-height: 5ch;
+  min-width: 4ch;
+  min-height: 4ch;
   position: relative;
   right: ${props => props.theme.right}px;
   bottom: ${props => props.theme.bottom}px;
@@ -78,17 +97,28 @@ const AnimatedSvg = styled.div`
   animation: ${props => props.theme.animation};
 `;
 
-function responsiveDistance(distance) {
+function responsiveWidth(distance) {
   let ratio = window.innerWidth / 1920;
-  ratio = ratio < 0.3 ? 0.3 : ratio;
+  ratio = ratio < 0.2 ? 0.2 : ratio;
   return ratio * distance;
 }
 
+function responsiveHeight(distance) {
+  const ratio = window.innerHeight / 1920;
+  return ratio * distance;
+}
+
+function responsiveDuration(duration) {
+  let ratio = window.innerWidth / 1920;
+  ratio = ratio < 0.4 ? 0.4 : ratio;
+  return ratio * duration;
+}
+
 function svgTheme(index) {
-  const radius = 20 + Math.random() * 10;
+  const radius = responsiveWidth(20 + Math.random() * 10);
   const direction = Math.random() > 0.5 ? -1 : 1;
   const startAngle = Math.random() * 360;
-  const duration = 2 + Math.random() * 3;
+  const duration = responsiveDuration(2 + Math.random() * 3);
   const keyFrame = keyframes`
     0% {
       transform:  rotate(${startAngle}deg)
@@ -104,10 +134,49 @@ function svgTheme(index) {
   const svgStyle = {};
   svgStyle['background-image'] = items[index];
   svgStyle.animation = css`${keyFrame} ${duration}s linear infinite`;
-  svgStyle.right = responsiveDistance(distanceX[index]);
-  svgStyle.bottom = responsiveDistance(distanceY[index]);
+  svgStyle.right = responsiveWidth(distanceX[index]);
+  svgStyle.bottom = responsiveHeight(distanceY[index]);
   return svgStyle;
 }
+
+const Intro = (props) => {
+  const { classes } = props;
+  const ref = useRef([]);
+  const [stateItems, set] = useState([]);
+  const transitions = useTransition(stateItems, null, {
+    from: {
+      opacity: 0, height: 0, innerHeight: 0, color: '#8fa5b6',
+    },
+    enter: [
+      { opacity: 1, height: 80, innerHeight: 60 },
+    ],
+    leave: [{ color: '#c23369' }, { innerHeight: 0 }, { opacity: 0, height: 0 }],
+    update: { color: '#28b4d7' },
+  });
+
+  const reset = useCallback(() => {
+    ref.current.map(clearTimeout);
+    ref.current = [];
+    set([]);
+    ref.current.push(setTimeout(() => set(['This is ', 'Software', 'Development', 'Club', 'IIT Delhi']), 3000));
+    ref.current.push(setTimeout(() => set(['This is ', 'Development', 'Club', 'IIT Delhi']), 4000));
+    ref.current.push(setTimeout(() => set(['This is ', 'Club', 'IIT Delhi']), 5000));
+    ref.current.push(setTimeout(() => set(['This is ', 'DevClub', 'IIT Delhi']), 8000));
+    ref.current.push(setTimeout(() => set(['This is ', 'DevClub']), 11000));
+  }, []);
+
+  useEffect(() => reset(), []);
+
+  return (
+    <div className={classes.intro}>
+      {transitions.map(({ item, props: { innerHeight, ...rest }, key }) => (
+        <animated.div className={classes.transitionsItem} key={key} style={rest} onClick={reset}>
+          <animated.div className={classes.transitionsText}>{item}</animated.div>
+        </animated.div>
+      ))}
+    </div>
+  );
+};
 
 const Home = (props) => {
   const { classes } = props;
@@ -124,22 +193,12 @@ const Home = (props) => {
     <React.Fragment>
       <div className={classes.root}>
         <Grid container className={classes.landingContainer}>
-          <Grid container item md={7} className={classes.container}>
+          <Grid container item md={7} className={classes.svgContainer}>
             <div className={classes.devices} />
             {svgArray}
           </Grid>
-          <Grid container item md={5}>
-            <Typing className={classes.typing}>
-              <Typing.Speed ms={60} />
-              <span className={classes.line1}>This is</span>
-              <Typing.Delay ms={200} />
-              <br />
-              <Typing.Speed ms={5} />
-              <span>The Software Development Club, IIT Delhi</span>
-              <Typing.Reset count={1} delay={800} />
-              <Typing.Speed ms={20} />
-              DevClub
-            </Typing>
+          <Grid container item md={5} className={classes.introContainer}>
+            <Intro classes={classes} />
           </Grid>
         </Grid>
       </div>
@@ -149,6 +208,10 @@ const Home = (props) => {
 };
 
 Home.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+Intro.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
